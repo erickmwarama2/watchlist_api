d4e49d81ade4bddee302ede25442e4699ed2e823
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2025-06-12 16:05:07 +0300

    initialize project

---
+++ watchlist_app/views.py
@@ -0,0 +1,13 @@
+# from django.shortcuts import render
+# from watchlist_app.models import Movie
+# from django.http import JsonResponse
+
+# def movie_list(request):
+#     movies = Movie.objects.all()
+#     data = {'movies': list(movies.values())}
+#     return JsonResponse(data=data)
+
+# def movie_detail(request, pk):
+#     movie = Movie.objects.get(pk=pk)
+#     data = {'movie': movie.name, 'description': movie.description, 'active': movie.active}
+#     return JsonResponse(data=data)
