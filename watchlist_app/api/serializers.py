b7f576c18c30ad8d5b43a43a85a61b8a882c0781
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2025-06-12 18:03:10 +0300

    add name validators

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
 
+def validate_name_length(value):
+    if len(value) <= 2:
+        raise serializers.ValidationError('Name is too short')
+
+    return value
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    name = serializers.CharField(validators=[validate_name_length])
     description = serializers.CharField()
     active = serializers.BooleanField()
 
@@ -16,3 +22,15 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
+
+    # def validate_name(self, value):
+    #     if len(value) <= 2:
+    #         raise serializers.ValidationError('Name is too short')
+
+    #     return value
+
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError('Name and description should be different')
+
+        return data
