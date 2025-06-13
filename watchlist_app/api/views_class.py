2af5a4daac39b4e0fa0b4a5e69729ac78e1c3fe2
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2025-06-13 15:21:05 +0300

    add custom permissions to reviews

--- watchlist_app/api/views_class.py
+++ watchlist_app/api/views_class.py
@@ -1,16 +1,20 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 from rest_framework import status, generics, mixins
+from rest_framework.permissions import IsAuthenticated
 
 # from watchlist_app.api.serializers import MovieSerializer
 from watchlist_app.api.model_serializers import (StreamPlatformSerializer,
                                                  WatchListSerializer, ReviewSerializer)
 from watchlist_app.models import WatchList, StreamPlatform, Review
+from watchlist_app.api.permissions import AdminOrReadOnly, ReviewUserOrReadOnly
 
 
 class ReviewList(generics.ListAPIView):
     # queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         pk = self.kwargs['pk']
@@ -19,18 +23,25 @@ class ReviewList(generics.ListAPIView):
 
 class ReviewCreate(generics.CreateAPIView):
     serializer_class = ReviewSerializer
+    queryset = Review.objects.all()
 
     def perform_create(self, serializer):
         pk = self.kwargs.get('pk')
         watchlist = WatchList.objects.get(pk=pk)
         # serializer.watchlist = watchlist
-        serializer.save(watchlist=watchlist)
+        review_user = self.request.user
+        review_queryset = Review.objects.filter(watchlist=watchlist, user=review_user)
+
+        if review_queryset.exists():
+            raise ValidationError('You have already reviewd this watchlist')
+
+        serializer.save(watchlist=watchlist, user=review_user)
 
 
 class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-
+    permission_classes = [ReviewUserOrReadOnly]
 
 # class ReviewDetail(mixins.RetrieveModelMixin, generics.GenericAPIView):
 #     queryset = Review.objects.all()
